87acd6a620991a85133c7a64748747455cd585f8
elinachar <elinachar@gmail.com>
2020-06-30 13:34:37 +0300

    Modify images, font

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // Smooth scrolling on single-page
   var $root = $('html, body');
-  $('.navbar-nav li a, .navbar-header a, .logo a, .contact-link').click(function() {
+  $('.navbar-nav li a, .navbar-header a, .logo a, .contact-link, .scroll-top a ').click(function() {
     var href = $.attr(this, 'href');
     if (href != undefined && href != '#') {
       var offset = top_offset()
@@ -14,17 +14,43 @@ $(document).ready(function(){
     };
   });
 
-  // Hover effect on Navbar
-  $(".navbar-default .navbar-nav>li>a").hover(function(){
-    var thisText = $(this).html();
-    var hoverText = "&#8729;" + thisText.slice(6,-6) + "&#8729;";
-    $(this).html(hoverText);
-  }, function(){
-    var thisText = $(this).html();
-    thisText = thisText.replace(/^.(.*).$/m, '$1');
-    var unhoverText = "&nbsp;" + thisText + "&nbsp;";
-    $(this).html(unhoverText);
+  // Fix Navbar on top
+  // Fix Navbar on top for md and lg screens
+  // Unfix Navbar for xs and sm screens
+  toggleNavbarFixOnTop();
+
+  $(window).on('resize', function() {
+    toggleNavbarFixOnTop();
   });
+
+  function toggleNavbarFixOnTop() {
+    if ($(window).width() > 767) {
+      $("nav").addClass("navbar-fixed-top");
+    } else {
+      $("nav").removeClass("navbar-fixed-top");
+    }
+  }
+
+  // Scroll on top button
+  //Get the button:
+  scrollTopBtn = document.getElementById("scroll-top-btn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollTopBtn.style.display = "block";
+      // If button is close to footer should stay above footer
+      if($(window).scrollTop() + $(window).height() > $(document).height() - 40) {
+        // Stop scroll-top-btn 3px upper than footer
+        var bottomOffset = $("footer").height() + 3;
+        $(scrollTopBtn).css("bottom", bottomOffset );
+      }
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  }
 
   //Parallax effect
   // resize the image(s) on page load
@@ -41,6 +67,55 @@ $(document).ready(function(){
     }
   };
 
+  // Typewriter effect
+  // Function for tooggling only one event with vendor prefix when animation is
+  // finished
+  function whichAnimationEvent(){
+    var t,
+        el = document.createElement("fakeelement");
+
+    var animations = {
+      "animation"      : "animationend",
+      "OAnimation"     : "oAnimationEnd",
+      "MozAnimation"   : "animationend",
+      "WebkitAnimation": "webkitAnimationEnd"
+    }
+
+    for (t in animations){
+      if (el.style[t] !== undefined){
+        return animations[t];
+      }
+    }
+  };
+
+  // Infinte typeWriter function and looping in job descriptions
+  function typeWriter() {
+    if (k < typewriterText[j].length) {
+      if (k == 0) {
+        $("#typewriter").text(typewriterText[j].charAt(k));
+      } else {
+        $("#typewriter").append(typewriterText[j].charAt(k));
+      }
+      k++;
+      // setTimeout(typeWriter, 100, j);
+      setTimeout(typeWriter, 85, j);
+    } else {
+      j++;
+      if (j == 3) {
+        j = 0;
+      }
+      k = 0;
+
+      setTimeout(typeWriter, 900);
+    }
+  }
+
+  // Call infine typeWriter animation
+  var typewriterText = ["Στα 10 μαθήματα νοιώθεις τη διαφορά ...", "... στα 20 τη βλέπουν όλοι ...", "... στα 30 έχεις ένα νέο σώμα!"];
+  var k = 0, j = 0;
+  typeWriter(j);
+
+
   //Photos Modal
   var slideIndex = 1;
 
@@ -50,13 +125,12 @@ $(document).ready(function(){
     $('#myModal').show()
     slideIndex = parseInt($(this).attr("data-slide"));
     showSlides(slideIndex);
-
   })
 
   // Close the Modal
   $(".modal .close").click(function() {
     $(".navbar").show();
-    show_hide_logo() 
+    show_hide_logo()
     $('#myModal').hide();
   })
 
@@ -93,15 +167,33 @@ $(document).ready(function(){
 
 // Function for navbar offset
 function top_offset() {
-  var offset = 50;
+  var offset = 0;
   var screenWidth = $(document).width();
   if (screenWidth >= 768) {
     var hLogo = $(".logo").height();
     var hNavbar = $(".navbar").height();
-    offset = hLogo + hNavbar;
+    offset = hLogo + hNavbar - 10;
   };
   return offset
 }
+// After document is loaded
+$(window).load(function(){
+  // After Google maps have been loaded
+  // Add alt tags to all of it images for SEO needs.
+  altImgMap();
+  function altImgMap() {
+    if (typeof google === 'object' && typeof google.maps === 'object') {
+      // Get all the images in the google map
+      var googleMapImages = $("#map img");
+      // Check which images do not have alt attribute and add an empty one
+      googleMapImages.each(function( index, value ) {
+        if (typeof $(value).attr("alt")== typeof undefined || $(value).attr("alt") == false) {
+          $(value).attr("alt" , "Google Maps");
+        }
+      })
+    }
+  };
+});
 
 // Parallax effect
 // resize the image(s) on page resize
@@ -112,9 +204,9 @@ $(window).on('resize', function(){
 // Parallax effect (https://inkplant.com/code/responsive-parallax-images)
 // keep all of your resize function calls in one place so you don't have to edit them twice (on page load and resize)
 function resize_all_parallax() {
-  var lDivId = ['index-section', 'bkg-2', 'bkg-3']; /* the ID of the div that you're resizing */
-  var lImgW = [1920, 1920, 1477]; /* the width of your image, in pixels */
-  var lImgH = [1080, 1080, 831]; /* the height of your image, in pixels */
+  var lDivId = ['bkg-1', 'bkg-2']; /* the ID of the div that you're resizing */
+  var lImgW = [1920, 1477]; /* the width of your image, in pixels */
+  var lImgH = [1080, 831]; /* the height of your image, in pixels */
 
   for (i = 0; i < lDivId.length; i++) {
 	  resize_parallax(lDivId[i], lImgW[i], lImgH[i]);
@@ -125,7 +217,7 @@ function resize_all_parallax() {
 function resize_parallax(divId, imgW, imgH) {
 	var div = $('#' + divId);
 	var divWidth = div.width();
-	if (divWidth < 769) { var pct = (imgH/imgW) * 100; } /* show full image, plus a little padding, if on static mobile view */
+	if (divWidth < 768) { var pct = (imgH/imgW) * 100; } /* show full image, plus a little padding, if on static mobile view */
 	else { var pct = 60; } /* this is the HEIGHT as percentage of the current div WIDTH. you can change it to show more (or less) of your image */
 	var newHeight = Math.round(divWidth * (pct/100));
 	newHeight = newHeight  + 'px';
@@ -165,7 +257,7 @@ $(window).on('resize', function(){
 
 function show_hide_logo() {
   var screenWidth = $(document).width();
-  if (screenWidth >= 769) {
+  if (screenWidth >= 768) {
     $(".logo").show();
   } else {
     $(".logo").hide();
@@ -175,7 +267,7 @@ function show_hide_logo() {
 // API Google Map
 function initMap() {
   // The location of position
-  var pos = {lat: 35.3260216, lng: 25.1214555};
+  var pos = {lat: 35.326293, lng: 25.123714};
   // The map, centered at position
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
